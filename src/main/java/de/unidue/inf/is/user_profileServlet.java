63c53aaf007b6649019dbe31096a09cd505d5530
Yazek <Yazek@Yazek-PC.local>
2018-01-09 01:23:34 +0100

    fixed errors maybe

--- src/main/java/de/unidue/inf/is/user_profileServlet.java
+++ src/main/java/de/unidue/inf/is/user_profileServlet.java
@@ -1,5 +1,17 @@
 package de.unidue.inf.is;
 
-public class user_profileServlet {
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import de.unidue.inf.is.domain.User;
+import de.unidue.inf.is.stores.UserStore;
+
+public final class user_profileServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
 
 }
