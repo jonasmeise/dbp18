f43b03ada4b528aa9ce3c4451bb9e4818af4009c
jonasmeise <jonas_meise@web.de>
2018-01-15 11:43:15 +0100

    saved

--- src/main/java/de/unidue/inf/is/utils/DBUtil.java
+++ src/main/java/de/unidue/inf/is/utils/DBUtil.java
@@ -2,6 +2,7 @@ package de.unidue.inf.is.utils;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Properties;
 
@@ -25,7 +26,11 @@ public final class DBUtil {
         }
     }
 
-
+    public static PreparedStatement prepareSQL(Connection myConnection, String commandSQL) throws SQLException {
+    	return myConnection.prepareStatement(commandSQL);
+    }
+    
+    
     public static Connection getConnection(String database) throws SQLException {
         final String url = "jdbc:db2:" + database;
         return DriverManager.getConnection(url);
