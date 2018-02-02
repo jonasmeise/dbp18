0ba16c1fe19820c42c28723544348ccb09931e24
Yazek <Yazek@Yazek-PC.local>
2018-02-02 22:45:14 +0100

    ultra SQL in userProfile test 1

--- src/main/java/de/unidue/inf/is/user_profileServlet.java
+++ src/main/java/de/unidue/inf/is/user_profileServlet.java
@@ -176,15 +176,15 @@ public final class user_profileServlet extends HttpServlet {
 		} 
    		
    		//Babbles
-    	 try {
- 			myConnection = myDB.getConnection("babble");	//SELECT b.text,b.created,b.creator,b.id, count(lb.babble) AS likes FROM babble b JOIN LikesBabble lb ON b.id = lb.babble  WHERE lb.type = 'likes' AND b.creator = ? GROUP BY  b.text,b.created,b.creator,b.id ORDER BY b.id DESC
- 			PreparedStatement myPrepStatement = myConnection.prepareStatement("SELECT text,created,creator,id FROM babble WHERE creator = ? ORDER BY id DESC");
+    	 try {	//SELECT text,created,creator,id FROM babble WHERE creator = ? ORDER BY id DESC
+ 			myConnection = myDB.getConnection("babble");	//
+ 			PreparedStatement myPrepStatement = myConnection.prepareStatement("SELECT b.text,b.created,b.creator,b.id, count(lb.babble) AS likes FROM babble b, LikesBabble lb WHERE b.id = lb.babble AND lb.type = 'likes' AND b.creator = ? GROUP BY  b.text,b.created,b.creator,b.id ORDER BY b.id DESC");
  			myPrepStatement.setString(1, userID);
  			ResultSet resultSet = myPrepStatement.executeQuery();
  			
  	
  		while (resultSet.next()){					//resultSet.getString("likes").toString()
- 				babblelist.add(new Babble(resultSet.getString("creator").toString(),resultSet.getString("text").toString(),resultSet.getString("created").toString(),"","","",resultSet.getString("id"))); //ID klappt nicht zu übergeben
+ 				babblelist.add(new Babble(resultSet.getString("creator").toString(),resultSet.getString("text").toString(),resultSet.getString("created").toString(),resultSet.getString("likes").toString(),"","",resultSet.getString("id"))); //ID klappt nicht zu übergeben
  				request.setAttribute("babblelist", babblelist);
  		
  		}
