121140953ecb9ae52d59d32ce7b1a01f0ff03ceb
Yazek <Yazek@Yazek-PC>
2018-02-05 15:24:09 +0100

    i

--- src/main/java/de/unidue/inf/is/babble_topLikedServlet.java
+++ src/main/java/de/unidue/inf/is/babble_topLikedServlet.java
@@ -24,7 +24,7 @@ public final class babble_topLikedServlet extends HttpServlet  {
 	@Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Connection myConnection = null;
-		DBUtil myDB = null; //a
+		DBUtil myDB = null; //ab
 		List<Babble> babblelist = new ArrayList<>();
 		
 		UtilityClass utility= new UtilityClass();
