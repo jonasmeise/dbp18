1aacced389d01d008d76f1feca1c3b28c635b292
Yazek <Yazek@Yazek-PC>
2018-02-05 11:30:49 +0100

    asda

--- src/main/java/de/unidue/inf/is/user_profileServlet.java
+++ src/main/java/de/unidue/inf/is/user_profileServlet.java
@@ -182,7 +182,7 @@ public final class user_profileServlet extends HttpServlet {
     		 //Der geht auch aber ohne likes und rebabbles
     		 String oldString ="SELECT b.text,b.created,b.creator,b.id FROM babble b WHERE b.creator = ?";
     		 //kA wieso das wieder zu lang ist
-    		 String testString ="SELECT b.text,b.created,b.creator,b.id, count(lb.babble) AS likes FROM babble b INNER JOIN likesBabble lb ON lb.username=b.creator WHERE lb.type='like' AND b.creator = ? GROUP BY b.text,b.created,b.creator,b.id ";
+    		 String testString ="SELECT b.text,b.created,b.creator,b.id, count(lb.babble) AS likes FROM babble b INNER JOIN likesBabble lb ON lb.username=b.creator WHERE lb.type='like' AND b.creator = ? GROUP BY b.id ";
  			myConnection = myDB.getConnection("babble");	
  			PreparedStatement myBabbleStatement = myConnection.prepareStatement(testString);
  			myBabbleStatement.setString(1, userID);
