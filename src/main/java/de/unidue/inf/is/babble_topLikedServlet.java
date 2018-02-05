2f65aa7582a50901cf29d4a6a161030ccae6b1dd
Yazek <Yazek@Yazek-PC>
2018-02-05 14:11:55 +0100

    EZ Clap

--- src/main/java/de/unidue/inf/is/babble_topLikedServlet.java
+++ src/main/java/de/unidue/inf/is/babble_topLikedServlet.java
@@ -32,40 +32,7 @@ public final class babble_topLikedServlet extends HttpServlet  {
 			PreparedStatement myBabbleStatement = myConnection.prepareStatement(top5String);
 			ResultSet resultSet = myBabbleStatement.executeQuery();
 			
-	
-		while (resultSet.next()){
-		
-			String likesString ="SELECT b.id,count(lb.babble) AS likes FROM babble b INNER JOIN likesbabble lb ON b.id=lb.babble WHERE lb.type='like' AND b.id=? GROUP BY b.id ";
-			PreparedStatement myLikesStatement = myConnection.prepareStatement(likesString);
-			String likeCount = "0";
-			String dislikesString ="SELECT b.id,count(lb.babble) AS dislikes FROM babble b INNER JOIN likesbabble lb ON b.id=lb.babble WHERE lb.type='dislike' AND b.id=? GROUP BY b.id ";
-			PreparedStatement myDislikesStatement = myConnection.prepareStatement(dislikesString);
-			String dislikeCount = "0";
-			String rebabbleString ="SELECT rb.babble ,count(rb.babble) AS rebabbles FROM rebabble rb WHERE rb.babble=? GROUP BY rb.babble ";
-			PreparedStatement myRebabbleStatement = myConnection.prepareStatement(rebabbleString);
-			String rebabbleCount = "0";
-			
-			myLikesStatement.setString(1, resultSet.getString("id"));
-			myDislikesStatement.setString(1, resultSet.getString("id"));
-			myRebabbleStatement.setString(1, resultSet.getString("id"));
-			
-			ResultSet likesResultSet = myLikesStatement.executeQuery();
-			ResultSet dislikesResultSet = myDislikesStatement.executeQuery();
-			ResultSet rebabblesResultSet = myRebabbleStatement.executeQuery();
-			
-			while(likesResultSet.next()){
-				likeCount = likesResultSet.getString("likes");
-			}
-			while(dislikesResultSet.next()){
-				dislikeCount = dislikesResultSet.getString("dislikes");
-			}
-			while(rebabblesResultSet.next()){
-				rebabbleCount = rebabblesResultSet.getString("rebabbles");
-			}
-			babblelist.add(new Babble(resultSet.getString("creator").toString(),resultSet.getString("text").toString(),resultSet.getString("created").toString(),likeCount,dislikeCount,rebabbleCount,resultSet.getString("id")));
-				request.setAttribute("babblelist", babblelist); 
-		}
-		
+			request.setAttribute("babblelist", myDB.createMetaData(myConnection, resultSet));	
  		} catch (SQLException e) {
  			// TODO Auto-generated catch block
  			e.printStackTrace();
